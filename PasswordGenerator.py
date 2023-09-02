82fdf540640047648359c3d1f9a9cb44173e4418
Bruno Leonardo <135679874+brunols7@users.noreply.github.com>
2023-09-02 17:46:37 -0300

    Add files via upload

---
+++ PasswordGenerator.py
@@ -0,0 +1,29 @@
+import random
+from time import sleep
+
+lower = "abcdefghijklmnopqrstuvwxyz"
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numb = "1234567890"
+simb = "!/@:;#$%&"
+
+char = lower + upper + numb
+
+print("=-"*22)
+print("Welcome to Password Generator - brunols7")
+print("=-"*22)
+
+sleep(1)
+
+pas_length = int(input("Enter the length of the password: "))
+print("=-"*20)
+
+sleep(1)
+
+print("Generating password...")
+print("=-"*20)
+
+sleep(1)
+
+password = "".join(random.sample(char, pas_length))
+print("Your password: ", password)
+print("=-"*20)
